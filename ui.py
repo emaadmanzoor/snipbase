83d76f16dc3914fbd65b588217e37646484afaec
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2014-09-14 04:45:34 -0400

    snipbase website v0.1

---
+++ ui.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import pymongo
+from pymongo import MongoClient
+
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    client = MongoClient('158.130.164.180')
+    db = client.dyna_database
+    clc = db.snippets_collection
+
+    snippets = [snippet for snippet in clc.find()]
+    print snippets
+
+    """
+    for id in db_res.keys():
+        all_res += [{"score" : db_res[id]["score"],
+                     "source": "Snipbase",
+                     "snippet": db_res[id]["payload"]["snippet"],
+                     "title": db_res[id]["payload"]["title"]}]
+    """
+
+    return render_template('index.html', snippets=snippets)
+
+if __name__ == "__main__":
+    app.run(debug=True)
